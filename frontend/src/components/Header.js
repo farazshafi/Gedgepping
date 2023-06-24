44c7b92ffd64154cf9e64a9499a78cede63f07df
Faraz shafi <95197056+farazshafi@users.noreply.github.com>
2023-06-24 20:03:17 +0530

    Implement react router

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,27 +1,31 @@
-import React from 'react'
-import { Navbar, Nav, Container } from 'react-bootstrap'
+import React from 'react';
+import { Navbar, Nav, Container } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
   return (
     <header>
       <Navbar bg='dark' variant='dark' expand='lg' collapseOnSelect>
         <Container>
-          <Navbar.Brand href='/'>ProShop</Navbar.Brand>
+          <Link to='/'>
+            <Navbar.Brand>Gedgepping</Navbar.Brand>
+          </Link>
           <Navbar.Toggle aria-controls='basic-navbar-nav' />
           <Navbar.Collapse id='basic-navbar-nav'>
             <Nav className='ml-auto'>
-              <Nav.Link href='/cart'>
+              <Link to='/cart' className='nav-link'>
                 <i className='fas fa-shopping-cart'></i> Cart
-              </Nav.Link>
-              <Nav.Link href='/login'>
+              </Link>
+              <Link to='/login' className='nav-link'>
                 <i className='fas fa-user'></i> Sign In
-              </Nav.Link>
+              </Link>
             </Nav>
           </Navbar.Collapse>
         </Container>
       </Navbar>
     </header>
-  )
-}
+  );
+};
 
-export default Header
+
+export default Header;
