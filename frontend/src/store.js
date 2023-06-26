23d990691edd743ebdb6a780e2bb63ca52d0bc79
Faraz shafi <95197056+farazshafi@users.noreply.github.com>
2023-06-26 12:34:37 +0530

    Shopping cart functionality

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -2,15 +2,25 @@ import { configureStore } from '@reduxjs/toolkit';
 import { combineReducers } from 'redux';
 import thunk from 'redux-thunk';
 import { productListReducer, productDetailsReducer } from "./reducers/productReducers"
-
+import {cartReducer} from "./reducers/cartReducers"
 const rootReducer = combineReducers({
   productList: productListReducer,
   productDetails: productDetailsReducer,
+  cart: cartReducer,
 });
+
+const cartItemsFromStorage = localStorage.getItem('cartItems')
+  ? JSON.parse(localStorage.getItem('cartItems'))
+  : []
+
+const initialState = {
+  cart: { cartItems: cartItemsFromStorage },
+}
 
 const store = configureStore({
   reducer: rootReducer,
   middleware: [thunk],
+  initialState
 });
 
 export default store;
