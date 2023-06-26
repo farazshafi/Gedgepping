c1406def0106a84cc36dc9d4619944acd79b2bf7
Faraz shafi <95197056+farazshafi@users.noreply.github.com>
2023-06-26 11:09:49 +0530

    Product list reducer, action & HomeScreen

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,8 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { combineReducers } from 'redux';
 import thunk from 'redux-thunk';
+import { productListReducer } from "./reducers/productReducers"
 
-const rootReducer = combineReducers({});
+const rootReducer = combineReducers({
+  productList: productListReducer
+});
 
 const store = configureStore({
   reducer: rootReducer,
