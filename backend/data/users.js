9b9da4cd3d7deb3b941bb76c163bd608839ed6df
Faraz shafi <95197056+farazshafi@users.noreply.github.com>
2023-09-02 12:32:10 +0530

    deleted all users , orders and DATA

--- backend/data/users.js
+++ backend/data/users.js
@@ -7,16 +7,6 @@ const users = [
     password: bcrypt.hashSync('123456', 10),
     isAdmin: true,
   },
-  {
-    name: 'Faraz Shafi',
-    email: 'farazpachu777@gmail.com',
-    password: bcrypt.hashSync('PachuGedgepping@123#&*$', 10),
-  },
-  {
-    name: 'test',
-    email: 'test@example.com',
-    password: bcrypt.hashSync('test', 10),
-  },
 ]
 
 export default users
